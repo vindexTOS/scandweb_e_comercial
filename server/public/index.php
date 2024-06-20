7a6474006c8aa46f01ba6310e18509b85df811d5
vindexTOS <giorgikutateladze1998@gmail.com>
2024-06-20 17:40:47 +0400

    added graphql models

--- server/public/index.php
+++ server/public/index.php
@@ -3,25 +3,26 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->get('/hello', [App\Controller\Test::class, 'test']);
+    
     $r->post('/graphql', [App\Controller\GraphQL::class, 'handle']);
 });
 
-$routeInfo = $dispatcher->dispatch(
-    $_SERVER['REQUEST_METHOD'],
-    $_SERVER['REQUEST_URI']
-);
+$routeInfo = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
 
 switch ($routeInfo[0]) {
     case FastRoute\Dispatcher::NOT_FOUND:
-        // ... 404 Not Found
+        http_response_code(404);
+        echo json_encode(['error' => 'Route not found']);
         break;
-    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-        $allowedMethods = $routeInfo[1];
-        // ... 405 Method Not Allowed
-        break;
-    case FastRoute\Dispatcher::FOUND:
-        $handler = $routeInfo[1];
-        $vars = $routeInfo[2];
-        echo $handler($vars);
-        break;
-}
+        case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+            $allowedMethods = $routeInfo[1];
+            http_response_code(405);
+            echo json_encode(['error' => 'Method not allowed']);
+            break;
+            case FastRoute\Dispatcher::FOUND:
+                $handler = $routeInfo[1];
+                $vars = $routeInfo[2];
+                echo $handler($vars);  
+                break;
+            }
