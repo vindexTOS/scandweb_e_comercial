a4592a0d80e80b9bf20b9b8d85feabf0ae4149ca
vindexTOS <giorgikutateladze1998@gmail.com>
2024-06-26 17:25:32 +0400

    added creatPrice reslover

--- server/src/Controller/GraphQL.php
+++ server/src/Controller/GraphQL.php
@@ -24,6 +24,18 @@ class GraphQL {
                 ],
             ]);
             
+            
+            
+            $priceType = new ObjectType([
+                "name" => "Price",
+                "fields"=>[
+                    'id' => Type::nonNull(Type::id()),
+                    'amount' => Type::nonNull(Type::int()),
+                    "currency" => Type::nonNull(Type::int()),
+                    "product_id" => Type::nonNull(Type::int())
+                    
+                ],
+            ]);
             // Define Query Type
             $queryType = new ObjectType([
                 'name' => 'Query',
@@ -73,38 +85,55 @@ class GraphQL {
                             ];
                         },
                     ],
-                ],
-            ]);
-            
-            // Create the Schema
-            $schema = new Schema(
-                (new SchemaConfig())
-                ->setQuery($queryType)
-                ->setMutation($mutationType)
-            );
-            
-            // Handle the GraphQL request
-            $rawInput = file_get_contents('php://input');
-            if ($rawInput === false) {
-                throw new RuntimeException('Failed to get php://input');
+                    "createPrice"=>[
+                        "type"=>$priceType ,
+                        "args"=>[
+                            'amount' => Type::nonNull(Type::int()),
+                            "currency" => Type::nonNull(Type::int()),
+                            "product_id" => Type::nonNull(Type::int())
+                        ],
+                        "resolve"=> static function ($rootValue, array $args){
+                            $db = new PDO("mysql:host=localhost;dbname=scandweb", "root", "");
+                            $stmt = $db->prepare("INSERT INTO price (amount, currency,product_id) VALUES (:amount, :currency, :product_id)");
+                            $stmt->bindParam(':label', $args['label']);
+                            $stmt->bindParam(':symbol', $args['symbol']);
+                            $stmt->execute();
+                            
+                            $currencyId = $db->lastInsertId();
+                        }
+                        ]
+                    ],
+                ]);
+                
+                // Create the Schema
+                $schema = new Schema(
+                    (new SchemaConfig())
+                    ->setQuery($queryType)
+                    ->setMutation($mutationType)
+                );
+                
+                // Handle the GraphQL request
+                $rawInput = file_get_contents('php://input');
+                if ($rawInput === false) {
+                    throw new RuntimeException('Failed to get php://input');
+                }
+                
+                $input = json_decode($rawInput, true);
+                $query = $input['query'];
+                $variableValues = $input['variables'] ?? null;
+                
+                $rootValue = ['prefix' => 'You said: '];
+                $result = GraphQLBase::executeQuery($schema, $query, $rootValue, null, $variableValues);
+                $output = $result->toArray();
+            } catch (Throwable $e) {
+                $output = [
+                    'error' => [
+                        'message' => $e->getMessage(),
+                    ],
+                ];
             }
             
-            $input = json_decode($rawInput, true);
-            $query = $input['query'];
-            $variableValues = $input['variables'] ?? null;
-            
-            $rootValue = ['prefix' => 'You said: '];
-            $result = GraphQLBase::executeQuery($schema, $query, $rootValue, null, $variableValues);
-            $output = $result->toArray();
-        } catch (Throwable $e) {
-            $output = [
-                'error' => [
-                    'message' => $e->getMessage(),
-                ],
-            ];
+            header('Content-Type: application/json; charset=UTF-8');
+            echo json_encode($output);
         }
-        
-        header('Content-Type: application/json; charset=UTF-8');
-        echo json_encode($output);
     }
-}
