902edc778bc2961658f0fc3c65a740ab5c66951f
vindexTOS <giorgikutateladze1998@gmail.com>
2024-07-17 17:49:05 +0400

    added card component for each item

---
+++ client/src/components/ProductCard.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Product } from "../Types/ProductsInterface";
+
+export default class ProductCard extends Component<Product> {
+  render() {
+    return <div>ProductCard</div>;
+  }
+}
